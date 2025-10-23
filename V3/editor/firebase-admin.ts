61ffd0c965197fd1bb607d9f02663bac886978b2
Arbitrage007 <nathnaelh5@gmail.com>
2025-10-23 05:12:20 +0300

    firebaseadmin

--- V3/editor/firebase-admin.ts
+++ V3/editor/firebase-admin.ts
@@ -1,13 +1,21 @@
 import { App, cert, getApp, getApps, initializeApp } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 
-const servicekey = require("@/service_key.json");
+// Deleted:const servicekey = require("@/service_key.json");
 
 let app: App;
 
 if (getApps().length === 0) {
+    const serviceKey = process.env.FIREBASE_SERVICE_ACCOUNT_KEY
+        ? JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY)
+        : undefined;
+
+    if (!serviceKey) {
+        throw new Error("Firebase service account key is not configured");
+    }
+
     app = initializeApp({
-        credential: cert(servicekey),
+        credential: cert(serviceKey),
     });
 }
 else {
@@ -17,4 +25,3 @@ else {
 const adminDB = getFirestore(app);
 
 export { app as adminApp, adminDB };
-
